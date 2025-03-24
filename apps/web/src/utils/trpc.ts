d5087d4ebb1877842f45ac2948e5e61fcfa3a33b
Mark Stevens <stevens.mark.a@gmail.com>
2025-03-24 17:55:41 -0400

    .

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -2,5 +2,5 @@
 
 import type { AppRouter } from '../../../api/src/index';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
- 
+
 export const { TRPCProvider, useTRPC, useTRPCClient } = createTRPCContext<AppRouter>();
