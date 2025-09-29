e7aecb19a1ee250d9bf20eaa17308a8df21c6134
Mark Stevens <stevens.mark.a@gmail.com>
2025-09-29 17:34:40 -0400

    Fix crash in flags when user is logged out

--- apps/backend/src/modules/featureFlags/featureFlags.router.ts
+++ apps/backend/src/modules/featureFlags/featureFlags.router.ts
@@ -2,16 +2,16 @@ import { publicProcedure, router } from "../../trpc";
 import { getUserFeatureFlagsMap } from "../../pkg/util/featureFlags";
 import { z } from "zod";
 import { Context } from "hono";
-import { getUserId } from "@/pkg/middleware/clerk-auth";
+import { getAuth, getUserId } from "@/pkg/middleware/clerk-auth";
 
 export const featureFlagsRouter = router({
   getUserFlags: publicProcedure
     .output(z.record(z.boolean()))
     .query(async ({ ctx }) => {
-      const userId = getUserId(ctx as Context);
-      if (!userId) return {};
+      const auth = getAuth(ctx as Context);
+      if (!auth?.userId) return {};
       
-      const flagsMap = await getUserFeatureFlagsMap(userId);
+      const flagsMap = await getUserFeatureFlagsMap(auth.userId);
       return Object.fromEntries(flagsMap);
     }),
 
@@ -19,10 +19,10 @@ export const featureFlagsRouter = router({
     .input(z.object({ flagName: z.string() }))
     .output(z.boolean())
     .query(async ({ ctx, input }) => {
-      const userId = getUserId(ctx as Context);
-      if (!userId) return false;
+      const auth = getAuth(ctx as Context);
+      if (!auth?.userId) return false;
       
-      const flagsMap = await getUserFeatureFlagsMap(userId);
+      const flagsMap = await getUserFeatureFlagsMap(auth.userId);
       return flagsMap.get(input.flagName) ?? false;
     }),
 }); 
